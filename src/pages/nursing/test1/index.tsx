4527f73d8da8908f6b26fe4bea32a6109e194433
changmen1 <zxl17339811909@163.com>
2025-08-21 12:10:39 +0800

    Initial commit

---
+++ src/pages/nursing/test1/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Test1: FC = () => {
+  return (
+    <>
+      Test1
+    </>
+  )
+}
+
+export default Test1;
---
+++ src/pages/nursing/test2/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Test2: FC = () => {
+  return (
+    <>
+      Test2
+    </>
+  )
+}
+
+export default Test2;
